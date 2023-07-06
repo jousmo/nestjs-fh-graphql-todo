58ea8296c5ea48bb4b7007236e4505656e49104b
José Uscanga <jousmo@outlook.com>
2023-07-06 13:18:11 -0600

    feat: initial configure nestjs graphql and apollo studio playground

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,21 @@
+import { join } from 'node:path';
 import { Module } from '@nestjs/common';
+import { ItemsModule } from './items/items.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: false,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+      plugins: [ApolloServerPluginLandingPageLocalDefault({ embed: true })],
+    }),
+    ItemsModule,
+  ],
   controllers: [],
   providers: [],
 })
---
+++ src/items/items.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { CreateItemInput } from './dto/create-item.input';
+import { UpdateItemInput } from './dto/update-item.input';
+
+@Injectable()
+export class ItemsService {
+  create(createItemInput: CreateItemInput) {
+    console.log({ createItemInput });
+    return 'This action adds a new item';
+  }
+
+  findAll() {
+    return `This action returns all items`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} item`;
+  }
+
+  update(id: number, updateItemInput: UpdateItemInput) {
+    console.log({ updateItemInput });
+    return `This action updates a #${id} item`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} item`;
+  }
+}
