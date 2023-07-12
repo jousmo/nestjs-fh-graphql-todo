ff7d8b6d2f871aab59008066d51645694d7246c9
José Uscanga <jousmo@outlook.com>
2023-07-12 15:24:01 -0600

    feat: implement valid roles using resolvers users

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,25 +1,42 @@
-import { Resolver, Query, Mutation, Args, ID } from '@nestjs/graphql';
+import { Args, ID, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { UsersService } from './users.service';
 import { User } from './entities/user.entity';
 import { ValidRolesArg } from './dto/args/valid-roles.arg';
+import { UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { CurrentUser } from '../auth/decorators/current-user.decorator';
+import { ValidRolesEnum } from '../auth/enums/valid-roles.enum';
 
 @Resolver(() => User)
+@UseGuards(JwtAuthGuard)
 export class UsersResolver {
   constructor(private readonly usersService: UsersService) {}
 
   @Query(() => [User], { name: 'users' })
-  async findAll(@Args() validRolesArg: ValidRolesArg): Promise<User[]> {
+  async findAll(
+    @Args() validRolesArg: ValidRolesArg,
+    @CurrentUser([ValidRolesEnum.admin]) user: User,
+  ): Promise<User[]> {
+    console.log('User', { user });
     const { roles } = validRolesArg;
     return this.usersService.findAll(roles);
   }
 
   @Query(() => User, { name: 'user' })
-  async findOne(@Args('id', { type: () => ID }) id: string): Promise<User> {
+  async findOne(
+    @Args('id', { type: () => ID }) id: string,
+    @CurrentUser([ValidRolesEnum.admin]) user: User,
+  ): Promise<User> {
+    console.log('User', { user });
     return this.usersService.findOne(id);
   }
 
   @Mutation(() => User)
-  blockUser(@Args('id', { type: () => ID }) id: string) {
+  blockUser(
+    @Args('id', { type: () => ID }) id: string,
+    @CurrentUser([ValidRolesEnum.admin]) user: User,
+  ) {
+    console.log('User', { user });
     return this.usersService.blockUser(id);
   }
 }
